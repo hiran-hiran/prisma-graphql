e6e45336a2fd5c06933b0360a04fd61a0ded1ba0
hiran-hiran <aluco.hirata@gmail.com>
2021-07-18 18:16:33 +0900

    init

--- src/index.ts
+++ src/index.ts
@@ -1,72 +1,124 @@
-import { PrismaClient } from "@prisma/client";
-import express from "express";
+import { PrismaClient } from '@prisma/client';
+import express from 'express';
 
 const prisma = new PrismaClient();
 const app = express();
 
 app.use(express.json());
 
-app.get("/users", async (req, res) => {
-  // const result = TODO
-  // res.json(result)
+app.get('/users', async (req, res) => {
+	const result = await prisma.user.findMany();
+	res.json(result);
 });
 
 app.post(`/signup`, async (req, res) => {
-  const { name, email } = req.body;
-
-  // const result = TODO
-
-  // res.json(result)
+	const { name, email } = req.body;
+	const result = await prisma.user.create({
+		data: {
+			name,
+			email,
+		},
+	});
+	res.json(result);
 });
 
 app.post(`/post`, async (req, res) => {
-  const { title, content, authorEmail } = req.body;
-
-  // const result = TODO
-
-  // res.json(result)
+	const { title, content, authorEmail } = req.body;
+	const result = await prisma.post.create({
+		data: {
+			title,
+			content,
+			author: {
+				connect: {
+					email: authorEmail,
+				},
+			},
+		},
+	});
+	res.json(result);
 });
 
-app.put("/post/:id/views", async (req, res) => {
-  const { id } = req.params;
-
-  // const result = TODO
-
-  // res.json(result)
+app.put('/post/:id/views', async (req, res) => {
+	const { id } = req.params;
+	const result = await prisma.post.update({
+		where: {
+			id: Number(id),
+		},
+		data: {
+			viewCount: {
+				increment: 1,
+			},
+		},
+	});
+	res.json(result);
 });
 
-app.put("/publish/:id", async (req, res) => {
-  const { id } = req.params;
-
-  // const result = TODO
-
-  // res.json(result)
+app.put('/publish/:id', async (req, res) => {
+	const { id } = req.params;
+	const result = await prisma.post.update({
+		where: {
+			id: Number(id),
+		},
+		data: {
+			published: true,
+		},
+	});
+	res.json(result);
 });
 
-app.get("/user/:id/drafts", async (req, res) => {
-  const { id } = req.params;
-
-  // const result = TODO
-
-  // res.json(result)
+// prisma fluent api
+// https://www.prisma.io/docs/concepts/components/prisma-client/relation-queries#fluent-api
+app.get('/user/:id/drafts', async (req, res) => {
+	const { id } = req.params;
+	const result = await prisma.user
+		.findUnique({
+			where: {
+				id: Number(id),
+			},
+		})
+		.posts({
+			where: {
+				published: false,
+			},
+		});
+	res.json(result);
 });
 
 app.get(`/post/:id`, async (req, res) => {
-  const { id } = req.params;
-
-  // const result = TODO
-
-  // res.json(result)
+	const { id } = req.params;
+	const result = await prisma.post.findUnique({
+		where: {
+			id: Number(id),
+		},
+	});
+	res.json(result);
 });
 
-app.get("/feed", async (req, res) => {
-  const { searchString, skip, take } = req.query;
+// skip, take
+// https://www.prisma.io/docs/concepts/components/prisma-client/pagination
+// searchString
+//
+app.get('/feed', async (req, res) => {
+	const { searchString, skip, take } = req.query;
+	console.log(searchString, skip, take);
 
-  // const result = TODO
+	const or = searchString
+		? {
+				OR: [
+					{ title: { contains: searchString as string } },
+					{ content: { contains: searchString as string } },
+				],
+		  }
+		: {};
 
-  // res.json(result)
+	const result = await prisma.post.findMany({
+		where: {
+			published: true,
+			...or,
+		},
+	});
+
+	res.json(result);
 });
 
-app.listen(3000, () =>
-  console.log(`🚀 Server ready at: http://localhost:3000`)
-);
+app.listen(3000, () => console.log(`🚀 Server ready at: http://localhost:3000`));
